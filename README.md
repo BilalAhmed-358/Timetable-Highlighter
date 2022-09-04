b6cc1e518dbe748ce2a0aa9a374b321b5cfb92ed
Bilal358 <ahmedkhanbilal358@gmail.com>
2022-09-04 10:44:52 +0500

    Added the Timetable highlighter bot

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Timetable-Highlighter
+# Timetable-Highlighter# Timetable-Highlighter 
+- This bot works only for the students of FAST NUCES university's timetable. 
+- You can highlight your section's timetable by changing the section's name in the source code
+## Documentation
+### Some limitations 
+    1. You must have python installed on your computer. 
+    2. You must have pyautogui library installed on your computer. 
+    3. This bot only works with screens that have resolution 1920x1080. 
+    4. Currently the section is randomly selected, you can put in your own sections in the source code and it'll work just fine.
+## Authors
+
+This bot was made as a fun side-project by Bilal Ahmed, I am currently a rising junior completing
+my degree in Computer Sciences at Fast University, Karachi. 
+
+Find me at: 
+- [Github](https://github.com/BilalAhmed-358) 
+- [LinkedIn](https://www.linkedin.com/in/ahmedkhanbilal/)
+- [Twitter](https://twitter.com/BilalAhmed_358) 
---
+++ Time Table Highlighter.py
@@ -0,0 +1,84 @@
+import pyautogui as py
+import time
+
+
+def writing_sec_name():
+   # Now entering text in the box
+    py.press('B')
+    py.press('C')
+    py.press('S')
+    py.press('-')
+    py.press('5')
+    py.press('B')
+
+
+def Selecting_and_opening_menu():
+    time.sleep(2)
+    py.hotkey('ctrl', 'a')
+
+    # conditional formatting coordinates are (557,74)
+    py.moveTo(557, 74)
+    py.click()
+
+    # highlight cell rules coordinates are (554,115)
+    py.moveTo(554, 115)
+    py.click()
+
+    # text that contains coordinates are (795,293)
+    py.sleep(1)
+    py.moveTo(795, 293)
+    py.click()
+    writing_sec_name()
+
+
+def Conditional_Formating_conditions():
+
+    # Selecting modifiction menu coordinates are (565,492)
+    py.moveTo(565, 492)
+    py.click()
+
+    # custom format coordinates are (532.586)
+    py.moveTo(532, 586)
+    py.click()
+
+    # selecing the Fill Tab coordinates are (351,229)
+    py.moveTo(351, 229)
+    py.click()
+
+    # red color coordiantes are (223,428)
+    py.moveTo(223, 428)
+    py.click()
+
+    # the okay button coordinates are (576,681)
+    py.moveTo(576, 681)
+    py.click()
+
+    # the other okay button coordinates are (566,524)
+    py.moveTo(566, 524)
+    py.click()
+    py.click()
+
+
+def highlight():
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday']
+    for i in days:
+        print("Highlighting "+i+" currently.")
+        # clikcing on the document
+        py.click(497, 522)
+        Selecting_and_opening_menu()
+        Conditional_Formating_conditions()
+        py.hotkey('ctrl', 'pagedown')
+    print("Now saving the work done...")
+    time.sleep(1)
+    py.hotkey('ctrl', 's')
+    print("Closing the file...")
+    time.sleep(1)
+    py.hotkey('alt', 'f4')
+
+
+highlight()
+print("Yay it worked!!!")
+time.sleep(1)
+print("Bye!")
+time.sleep(1)
+print("Bas itna hi tha, thanks for watching!")
